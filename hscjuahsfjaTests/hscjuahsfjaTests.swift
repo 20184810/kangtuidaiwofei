17511b933db29275930a67cdca588eaad5c2dac2
Zhenyang Yu <20184810@co-314-32.local>
2018-05-02 13:29:56 -0400

    Initial Commit

---
+++ hscjuahsfjaTests/hscjuahsfjaTests.swift
@@ -0,0 +1,36 @@
+//
+//  hscjuahsfjaTests.swift
+//  hscjuahsfjaTests
+//
+//  Created by Zhenyang Yu on 5/2/18.
+//  Copyright © 2018 Zhenyang Yu. All rights reserved.
+//
+
+import XCTest
+@testable import hscjuahsfja
+
+class hscjuahsfjaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
